e1e2134d1a9957fab240c1d31a1bc7eaa2fe6742
yashoda4A3 <ssdvy.p@gmail.com>
2018-04-06 22:03:26 +0530

    Login and Home Pages are added with navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
+import LoginPage from './components/pages/LoginPage';
 
-const App = () => <div><h1>Hello World!!!!</h1></div> 
+
+const App = () => (
+<div>
+    <Route path="/" exact component={HomePage}/>
+    <Route path="/login" exact component={LoginPage}/>
+</div> 
+);
 
 export default App;
